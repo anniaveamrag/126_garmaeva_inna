5b39d370554e9a55b1bcd18385a56909f0611fd8
anniaveamrag <90262412+anniaveamrag@users.noreply.github.com>
2021-10-26 17:05:51 +0300

    added new info

---
+++ task_2/src/Task_2.java
@@ -0,0 +1,40 @@
+public class Task_2 implements Task_2_base {
+    @Override
+    public int subtask_1_while(int num) {
+        int max = 1;
+        Scanner.in = new Scanner (System.in)
+                int num = scanner.nextInt();
+        (while max**2 < num){
+            int max = max++;
+        }
+        System.out.println(max**2);
+        }
+        // Найти максимальное число, являющееся полным квадратом,
+        // не превосходящее заданное натуральное num
+        return 0;
+    }
+
+    @Override
+    public int subtask_2_while(int num) {
+        // Последовательность целых чисел p(n) определяется следующим образом:
+        // p(0) = 1
+        // p(k) = 2 * p(k - 1) + 6, k > 0
+        //Найти элемент последовательности с номером num
+        return 0;
+    }
+
+    @Override
+    public boolean subtask_3_while(int num, int base) {
+        // Проверить, является ли число num натуральной степенью числа base
+        return false;
+    }
+
+    @Override
+    public int subtask_4_while(int start, int end) {
+        // Вычислить, за какое минимальное число операций
+        // вычесть 1
+        // поделить на 2
+        // число start можно превратить в end. Гарантируется, что start >= end >= 1
+        return 0;
+    }
+}
