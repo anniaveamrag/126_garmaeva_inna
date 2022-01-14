05170132ad72a0dbd03111e7949342ae0b51db33
anniaveamrag <90262412+anniaveamrag@users.noreply.github.com>
2022-01-14 20:54:11 +0300

    upd

--- task_10/src/test/SystemTest.java
+++ task_10/src/test/SystemTest.java
@@ -10,4 +10,31 @@ public class SystemTest extends Assertions {
         DocumentBook documentBook = DocumentBook.create();
         assertEquals(0,documentBook.getDocCount());
     }
+    @Test
+    public void addDoc_addDocWithNumberAndDate_DocCountEqualsOne(){
+        DocumentBook documentBook = DocumentBook.create();
+        documentBook.addDoc("number","date");
+        assertEquals(1,documentBook.getDocCount());
+    }
+    @Test
+    public void addDoc_addDocWithNullNumber_ThrowsException(){
+        DocumentBook documentBook = DocumentBook.create();
+        var exc = assertThrows(IllegalArgumentException.class, () -> documentBook.addDoc(null,"date"));
+        assertTrue(exc.getMessage().toLowerCase().contains("Номер документа должен содержать цифры"));
+    }
+    @Test
+    public void addDoc_addDocWithNullDate_ThrowsException(){
+        DocumentBook documentBook = DocumentBook.create();
+        var exc = assertThrows(IllegalArgumentException.class, () -> documentBook.addDoc("number",null));
+        assertTrue(exc.getMessage().toLowerCase().contains("Укажите правильную дату"));
+    }
+
+    @Test
+    public void addDoc_addDocWithNullNumberAndNullDate_ThrowsException(){
+        DocumentBook documentBook = DocumentBook.create();
+        var exc = assertThrows(IllegalArgumentException.class, () -> documentBook.addDoc(null,null));
+        assertTrue(exc.getMessage().toLowerCase().contains("Номер документа должен содержать цифры") &&
+                exc.getMessage().toLowerCase().contains("Укажите правильную дату"));
+    }
+
 }
