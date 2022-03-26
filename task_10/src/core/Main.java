ee3ec362c2298c0f8d282d88dace0e7f9e411818
anniaveamrag <90262412+anniaveamrag@users.noreply.github.com>
2022-03-26 23:27:05 +0300

    cleaning

--- task_10/src/core/Main.java
+++
@@ -1,8 +0,0 @@
-package core;
-
-public class Main {
-    public static void main(String[] args) {
-        DocumentBook documentBook = new DocumentBook();
-        documentBook.commandList();
-    }
-}
--- task_10/src/core/TypeOfPaymentDoc.java
+++
@@ -1,6 +0,0 @@
-package core;
-
-public enum TypeOfPaymentDoc {
-    PaymentOrder,
-    BankOrder
-}
